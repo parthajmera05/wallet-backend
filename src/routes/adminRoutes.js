5871c376f635f894971d5544d146d69dedbf90ff
Parth Ajmera <ajmeraparth05@gmail.com>
2025-05-22 14:05:55 +0530

    Added Wallet Routes and Admin Routes

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { getFlaggedTransactions, getTotalUserBalances, getTopUsers } from '../controllers/adminController.js';
+import { adminRegister, adminLogin } from '../controllers/adminAuthController.js';
+import { authenticateAdmin } from '../middlewares/authAdminMiddleware.js';
+
+const router = express.Router();
+
+
+router.post('/register', adminRegister);
+router.post('/login', adminLogin);
+
+
+router.get('/flagged-transactions', authenticateAdmin, getFlaggedTransactions);
+router.get('/total-balances', authenticateAdmin, getTotalUserBalances);
+router.get('/top-users', authenticateAdmin, getTopUsers);
+
+export default router;
--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { deposit, getAllBalances, getBalanceByCurrency, withdraw, getTransactions} from '../controllers/walletController.js';
+import { deposit, getAllBalances, getBalanceByCurrency, withdraw, getTransactions, transfer} from '../controllers/walletController.js';
 import { authenticateJWT } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/deposit', authenticateJWT, deposit);
 router.post('/withdraw', authenticateJWT, withdraw);
+router.post('/transfer', authenticateJWT, transfer);
 router.get('/balances', authenticateJWT, getAllBalances);
 router.get('/balance/:currency', authenticateJWT, getBalanceByCurrency);
 router.get('/transactions', authenticateJWT, getTransactions);
